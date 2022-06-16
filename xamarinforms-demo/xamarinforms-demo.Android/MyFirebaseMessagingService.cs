56536d88511190f8045972aa67a75c4af3e2dcc9
Kohei Yamamoto <e0457789@u.nus.edu>
2022-06-17 01:25:28 +0900

    Initial commit

---
+++ xamarinforms-demo/xamarinforms-demo.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,121 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Util;
+using Firebase.Messaging;
+using WindowsAzure.Messaging;
+using AndroidX.Core.App;
+
+namespace xamarinforms_demo.Droid
+{
+    [Service(Exported = true)]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        NotificationHub hub;
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            if (message.GetNotification() != null)
+            {
+                //These is how most messages will be received
+                Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+                SendNotification(message.GetNotification().Body);
+            }
+            else
+            {
+                //Only used for debugging payloads sent from the Azure portal
+                SendNotification(message.Data.Values.First());
+
+            }
+        }
+        void SendNotification(string messageBody)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID);
+
+            // edit Content Title for notification title
+            notificationBuilder.SetContentTitle("Push Demo Android")
+                        .SetSmallIcon(Resource.Drawable.ic_launcher)
+                        .SetContentText(messageBody)
+                        .SetAutoCancel(true)
+                        .SetShowWhen(false)
+                        .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+        public override void OnNewToken(string token)
+        {
+            Log.Debug(TAG, "FCM token: " + token);
+            SendRegistrationToServer(token);
+        }
+
+        void SendRegistrationToServer(string token)
+        {
+            // Register with Notification Hubs
+            hub = new NotificationHub(Constants.NotificationHubName,
+                                        Constants.ListenConnectionString, this);
+
+            var dt = DateTime.Now;
+            var timeTag = dt.ToString("HH:mm:ss");
+
+            var carVendor = "";
+            var vendorType = "";
+            var vendorLocation = "";
+            var carGrade = "";
+            switch (Convert.ToInt64(dt.Minute) % 10)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    carVendor = "TOYOTA";
+                    vendorType = "JapaneseCar";
+                    vendorLocation = "Aichi";
+                    carGrade = "20";
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    carVendor = "NISSAN";
+                    vendorType = "JapaneseCar";
+                    vendorLocation = "Kanagawa";
+                    carGrade = "15";
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    carVendor = "BMW";
+                    vendorType = "ForeignCar";
+                    vendorLocation = "Munich";
+                    carGrade = "10";
+                    break;
+                default:
+                    carVendor = "tmp1";
+                    vendorType = "tmp2";
+                    vendorLocation = "tmp3";
+                    carGrade = "tmp4";
+                    break;
+            }
+
+            var tags = new List<string>() { timeTag, carVendor, vendorType, vendorLocation, carGrade };
+            var regID = hub.Register(token, tags.ToArray()).RegistrationId;
+
+            Log.Debug(TAG, $"Successful registration of ID {regID}");
+        }
+    } 
+}
---
+++ xamarinforms-demo/xamarinforms-demo/MainPage.xaml.cs
@@ -0,0 +1,154 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace xamarinforms_demo
+{
+    public partial class MainPage : ContentPage
+    {
+        static readonly HttpClient client = new HttpClient();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void CurrentLocationButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // clear pins
+                map.Pins.Clear();
+                // get current location
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+                if (location != null)
+                {
+                    // set current location to gpslabel at the bottom of the screen
+                    GPSlabel.Text = $"{location.Latitude},{location.Longitude}";
+                    // enable IsShowingUser botton in the map
+                    map.IsShowingUser = true;
+                    // move the map to current location
+                    Position position = new Position(Convert.ToDouble(location.Latitude), Convert.ToDouble(location.Longitude));
+                    MapSpan mapSpan = new MapSpan(position, 0.015, 0.015);
+                    map.MoveToRegion(mapSpan);
+                    // get records from azure search
+                    HttpResponseMessage res = await client.GetAsync(Constants.SearchRequestURL + "*" + "&api-key=" + Constants.SearchRequestKey);
+                    string content = await res.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(content);
+                    // get records nearby
+                    var nearRecords = GetNearRecords(position, json);
+                    // add pins nearby
+                    foreach (JObject item in nearRecords)
+                    {
+                        AddPin(Convert.ToString(item.GetValue("NAME")), Convert.ToString(item.GetValue("DESCRIPTION")), new Position(Convert.ToDouble(item.GetValue("LAT")), Convert.ToDouble(item.GetValue("LONG"))));
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Location is not obtained.");
+            }
+        }
+        private async void SearchLocationButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // clear pins
+                map.Pins.Clear();
+                // get coordinates geocoded from entry.Text 
+                Geocoder geoCoder = new Geocoder();
+                IEnumerable<Position> approximateLocations = await geoCoder.GetPositionsForAddressAsync(entry.Text);
+                Position position = approximateLocations.FirstOrDefault();
+                if (position != null)
+                {
+                    // move the map to current location
+                    MapSpan mapSpan = new MapSpan(position, 0.015, 0.015);
+                    map.MoveToRegion(mapSpan);
+                    // if keyphrase is not specified
+                    if (string.IsNullOrEmpty(keyphrase.Text))
+                    {
+                        // set current location to gpslabel at the bottom of the screen
+                        GPSlabel.Text = $"{position.Latitude},{position.Longitude}";
+                        // get records from azure search
+                        HttpResponseMessage res = await client.GetAsync(Constants.SearchRequestURL + "*" + "&api-key=" + Constants.SearchRequestKey);
+                        string content = await res.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(content);
+                        // get records nearby
+                        var nearRecords = GetNearRecords(position, json);
+                        // get trend keyphrase
+                        Trend.Text = getTrendKeyphrase(nearRecords);
+                        // add pins nearby
+                        foreach (JObject item in nearRecords)
+                        {
+                            AddPin(item.GetValue("NAME").ToString(), item.GetValue("DESCRIPTION").ToString(), new Position(Convert.ToDouble(item.GetValue("LAT")), Convert.ToDouble(item.GetValue("LONG"))));
+                        }
+                    }
+                    else // if keyphrase s specified
+                    {
+                        // set current location to gpslabel at the bottom of the screen
+                        GPSlabel.Text = $"{position.Latitude},{position.Longitude}";
+                        // get records from azure search with keyphrase specified 
+                        HttpResponseMessage res = await client.GetAsync(Constants.SearchRequestURL + keyphrase.Text + "searchFields=keyphrases&api-key=" + Constants.SearchRequestKey);
+                        string content = await res.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(content);
+                        // get records nearby
+                        var nearRecords = GetNearRecords(position, json);
+                        // add pins nearby
+                        foreach (JObject item in nearRecords)
+                        {
+                            AddPin(item.GetValue("NAME").ToString(), item.GetValue("DESCRIPTION").ToString(), new Position(Convert.ToDouble(item.GetValue("LAT")), Convert.ToDouble(item.GetValue("LONG"))));
+                        }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Location cannot be obtained.");
+            }
+        }
+        private List<Object> GetNearRecords(Position position, JObject json)
+        {
+            var nearRecords = new List<Object>();
+            foreach (JObject item in json["value"])
+            {
+                if ((Math.Abs(position.Latitude - Convert.ToDouble(item.GetValue("LAT"))) <= 0.05) && (Math.Abs(position.Longitude - Convert.ToDouble(item.GetValue("LONG"))) <= 0.05))
+                {
+                    nearRecords.Add(item);
+                }
+            }
+            return nearRecords;
+        }
+        private string getTrendKeyphrase(List<object> nearRecords)
+        {
+            var listKeyphrase = new List<string>();
+            foreach (JObject item in nearRecords)
+            {
+                foreach (var kp in item["keyphrases"])
+                {
+                    listKeyphrase.Add(kp.ToString());
+                }
+            }
+            var mostCommon = (from item in listKeyphrase
+                              group item by item into g
+                              orderby g.Count() descending
+                              select g.Key).First();
+            return mostCommon;
+        }
+        private void AddPin(string name, string desc, Position position)
+        {
+            Pin pin = new Pin
+            {
+                Label = name,
+                Address = desc,
+                Type = PinType.Place,
+                Position = position
+            };
+            map.Pins.Add(pin);
+        }
+    }
+}
